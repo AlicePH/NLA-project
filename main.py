70b90c6421869575ee5cacf6a229665f46c81640
QuantumDragon2902 <50214145+QuantumDragon2902@users.noreply.github.com>
2022-12-22 08:05:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,56 +1,28 @@
-import torch
-import torchvision
-import torchvision.transforms as transforms
-import torch.nn as nn
-from collections import OrderedDict
-from models import CaffeBNAlexNet, CaffeBNLowRankAlexNet, CNN, CNNLowRank
-import time
-import datetime
+import argparse
+from src.train import train
+from src.test import test
 
-transform = transforms.Compose(
-                                [transforms.Resize((256,256)),
-                                 transforms.ToTensor(),
-                                 transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+def main():
+    parser = argparse.ArgumentParser()
+    arg = parser.add_argument
+    arg('--model', dest='model', help='Model name', required=True)
+    arg('--rank', dest='rank', help='Rank list', required=True)
+    arg('--scheme', dest='scheme', help='Scheme', required=True)
+    arg('--mode', dest='mode', help='Tarin or test mode', required=True)
 
-batch_size = 4
+    parser.set_defaults(model='CaffeBNAlexNet')
+    parser.set_defaults(weights=1)
+    parser.set_defaults(scheme='scheme_2')
+    args = parser.parse_args()
+    print(args.mode)
+    if args.mode == 'train':
+        time = train(args.model, args.rank, args.scheme)
+        print(time)
+    else:
+        test(args.model, args.rank, args.scheme)
 
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                        download=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
-                                          shuffle=True, num_workers=2)
 
-testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                       download=True, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
-                                         shuffle=False, num_workers=2)
 
-classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-
-model = CaffeBNAlexNet().to(device)
-
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=0.01, weight_decay = 0.001, momentum = 0.9) 
-
-start_time = time.time()
-for epoch in range(2): 
-    running_loss = 0.0
-    for i, data in enumerate(trainloader, 0):
-        inputs, labels = data[0].to(device), data[1].to(device)
-
-        optimizer.zero_grad()
-
-        outputs = model(inputs)
-
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-
-        running_loss += loss.item()
-        if i % 2000 == 1999:   
-            print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
-            running_loss = 0.0
-elapsed_time = time.time() - start_time
-print(str(datetime.timedelta(seconds=elapsed_time)))
-
+if __name__ == '__main__':
+    main()
